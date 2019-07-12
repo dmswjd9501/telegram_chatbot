ada9b83652f5df3c691bdf5d25fbc532ed052a5b
dmswjd9501 <dmswjd9501@soongsil.ac.kr>
2019-07-12 15:40:34 +0900

    chatbot

---
+++ telegram.py
@@ -0,0 +1,25 @@
+import requests
+import pprint
+from decouple import config # 파이썬에서 환경변수 관리하는 패키지
+
+# 1. 토큰 및 기본 url 설정
+token = config('TELEGRAM_TOKEN') # .env 설정값 가져오기
+base_url = f'https://api.telegram.org/bot{token}/'
+
+# 2. getUpdates 정보 가져오기
+response = requests.get(base_url+'getUpdates').json()
+# print(response)
+
+# 3. 나의 chat_id 가져오기
+# print(response.keys())
+chat_id = response.get('result')[0].get('message').get('chat').get('id')
+# 859082237
+
+# 4. chat_id에 메시지 보내기
+# 4-1. 요청 보낼 URL 만들기
+text = '메롱'
+api_url = f'{base_url}sendMessage?chat_id={chat_id}&text={text}'
+
+# 4.2 requests로 보내기
+requests.get(api_url)
+
